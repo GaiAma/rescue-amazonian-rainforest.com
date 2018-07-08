a586301fcccbf31241081aee5e78deb0b3c4fc61
CanRau <cansrau@gmail.com>
2018-07-07 20:08:08 -0500

    fix isPage regex

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const slugify = require(`slugify`)
 const { compose, groupWith, last, takeLast, head } = require(`ramda`)
 
 const isPage = node =>
-  /\/content\/pages\/[^_]+\/index\.[^/]+/.test(node.fileAbsolutePath)
+  /\/content\/(.*\/)?pages\/[^_]+\/index\.[^/]+/.test(node.fileAbsolutePath)
 
 const getNLast = n =>
   compose(
@@ -22,7 +22,6 @@ const getGroup = node => {
 const groupByPage = groupWith((a, b) => getGroup(a) === getGroup(b))
 
 exports.onCreateNode = ({ node, actions }) => {
-  isPage(node) && console.log(node)
   const { createNodeField } = actions
   createNodeField({
     node,
@@ -45,69 +44,69 @@ exports.onCreateNode = ({ node, actions }) => {
   }
 }
 
-// exports.createPages = async ({ actions, getNodes, graphql }) => {
-//   const { createPage, createNodeField } = actions
+exports.createPages = async ({ actions, getNodes, graphql }) => {
+  const { createPage, createNodeField } = actions
 
-//   // get pages and posts
-//   const graphNodes = await graphql(`
-//     {
-//       languages: allLanguagesYaml {
-//         edges {
-//           node {
-//             id
-//             title
-//           }
-//         }
-//       }
+  // get pages and posts
+  const graphNodes = await graphql(`
+    {
+      languages: allLanguagesYaml {
+        edges {
+          node {
+            id
+            title
+          }
+        }
+      }
 
-//       pages: allMarkdownRemark(
-//         filter: { fields: { isPage: { eq: true }, slug: { ne: null } } }
-//       ) {
-//         edges {
-//           node {
-//             fields {
-//               slug
-//             }
-//             frontmatter {
-//               lang
-//               layout
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
+      pages: allMarkdownRemark(
+        filter: { fields: { isPage: { eq: true }, slug: { ne: null } } }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              lang
+              layout
+            }
+          }
+        }
+      }
+    }
+  `)
 
-//   if (graphNodes.errors) {
-//     return Promise.reject(graphNodes.errors)
-//   }
+  if (graphNodes.errors) {
+    return Promise.reject(graphNodes.errors)
+  }
 
-//   // create pages & blog posts
-//   graphNodes.data.pages.edges.forEach(({ node }) => {
-//     createPage({
-//       path: node.fields.slug,
-//       component: resolve(`./src/page-templates/${node.frontmatter.layout}.js`),
-//       context: {
-//         slug: node.fields.slug,
-//         lang: node.frontmatter.lang,
-//       },
-//     })
-//   })
+  // create pages & blog posts
+  graphNodes.data.pages.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: resolve(`./src/page-templates/${node.frontmatter.layout}.js`),
+      context: {
+        slug: node.fields.slug,
+        lang: node.frontmatter.lang,
+      },
+    })
+  })
 
-//   const pages = getNodes().filter(isPage)
-//   // console.log(groupByPage(pages))
-//   groupByPage(pages).forEach(group =>
-//     group.forEach((node, index, array) =>
-//       createNodeField({
-//         node,
-//         name: `translations`,
-//         value: array.filter(n => n.id !== node.id).map(node => node.id),
-//       })
-//     )
-//   )
+  const pages = getNodes().filter(isPage)
+  // console.log(groupByPage(pages))
+  groupByPage(pages).forEach(group =>
+    group.forEach((node, index, array) =>
+      createNodeField({
+        node,
+        name: `translations`,
+        value: array.filter(n => n.id !== node.id).map(node => node.id),
+      })
+    )
+  )
 
-//   return true
-// }
+  return true
+}
 
 exports.onCreateBabelConfig = ({ actions: { setBabelPlugin } }) => {
   // setBabelPlugin({ name: `babel-plugin-tailwind` })
